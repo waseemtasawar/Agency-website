488f6df99e93f9e23f30eed6af1529d82cac8260
waseem tasawar <waseemtasawar97@gmail.com>
2024-03-31 12:39:50 +0500

    update json files

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="bg-neutralSilver">
+      <div className="px-4 lg:max-w-screen-2xl mx-auto min-h-screen h-screen">
+        <Carousel>
+          <div className="flex h-full items-center justify-center ">
+            Slide 1
+          </div>
+          <div className="flex h-full items-center justify-center ">
+            Slide 2
+          </div>
+          <div className="flex h-full items-center ">Slide 3</div>
+        </Carousel>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
